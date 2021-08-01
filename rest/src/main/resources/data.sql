6843b64dc9a6c6151dcf1231a5deea521812ec5b
diego <diego@diego-Lenovo-G480>
2021-08-01 19:46:29 -0300

    versao com H2 database

---
+++ rest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into nationalities values (0,'Desconhecido'), (1,'Brasileiro'), (2,'Argentino'), (3,'Urguaio');
+insert into places values (0,'Desconhecido'), (1,'Florianopolis, SC'), (2,'Porto Alegre, RS'), (3,'Curitiba, PR');
+insert into persons values 
+(229,'outra pessoa','123.123.123-12', 'email@domain.com', 1, 2, 2, '2000-11-11'), 
+(232,'nome pessoa', '123.123.123-11', 'email1@domain.com', 2, 2, 0,'2000-11-11');
